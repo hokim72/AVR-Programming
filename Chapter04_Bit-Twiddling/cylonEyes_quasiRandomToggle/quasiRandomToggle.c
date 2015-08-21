8e76ff73528207fee253484fe273b435bf8b30d7
Hyunok Kim <hokim72@gmail.com>
2015-08-22 05:39:46 +0900

    Add cylonEyes_quasiRandomToggle

---
+++ Chapter04_Bit-Twiddling/cylonEyes_quasiRandomToggle/quasiRandomToggle.c
@@ -0,0 +1,24 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DELAYTIME	45
+#define LED_PORT	PORTD
+#define	LED_DDR		PORTD
+
+int main(void) {
+
+	uint16_t x = 0x1234;
+	uint8_t y;
+	LED_DDR = 0xff;			/* Data Direction Register B:
+							   all set up for output */
+	
+	while (1) {
+
+		x = 2053 * x + 138449;		/* "random" number generator */
+		y = (x >> 8) & 0b00000111;	/* pick three bits from high byte */
+		LED_PORT ^= (1 << y);		/* toggle one bit */
+		_delay_ms(100);
+	}
+
+	return (0);
+}
