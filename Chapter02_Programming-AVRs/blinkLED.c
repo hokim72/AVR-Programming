dbfa101eecbead1439601cd50c3cb3abde26668e
Hyunok Kim <hokim72@gmail.com>
2015-08-23 10:45:14 +0900

    Update CMakeLists.txt

--- Chapter02_Programming-AVRs/blinkLED.c
+++ Chapter02_Programming-AVRs/blinkLED.c
@@ -4,16 +4,16 @@
 int main(void) {
 
 	// Inits
-	DDRB |= 0b00000001;		/* Data Direction Register D:
+	DDRD |= 0b00000001;		/* Data Direction Register D:
 							   writing a one to the bit
 							   enables output. */
 
 	// Event loop
 	while (1) {
-		PORTB = 0b00000001;		/* Turn on first LED bit/pin in PORTD */
+		PORTD = 0b00000001;		/* Turn on first LED bit/pin in PORTD */
 		_delay_ms(1000);									/* wait */
 
-		PORTB = 0b00000000;		/* Turn off all D pins, including LED */
+		PORTD = 0b00000000;		/* Turn off all D pins, including LED */
 		_delay_ms(1000);									/* wait */
 
 	}
