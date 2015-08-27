4f03f07c34782e0ec2e130c0ca09297cd74ad380
Hyunok Kim <hokim72@gmail.com>
2015-08-28 07:20:31 +0900

    Add slowScope

---
+++ Chapter07_Analog-to-Digital-Conversion-I/slowScope/slowScope.c
@@ -0,0 +1,36 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/power.h>
+#include "USART.h"
+
+#define SAMPLE_DELAY	50	
+
+static inline void initFreerunningADC(void) {
+	ADMUX |= (1 << REFS0);		/* reference voltage on AVCC */
+//	ADCSRA |= (1 << ADPS1) | (1 << ADPS0);	/* ADC clock prescaler /8 */
+	ADCSRA |= (1 << ADPS2);					/* ADC clock prescaler /16 */
+
+	//ADMUX |= (1 << ADLAR);	/* left-adjust result, return only 8bits */
+
+	ADCSRA |= (1 << ADEN);		/* enable ADC */
+	ADCSRA |= (1 << ADATE);		/* auto-trigger enable */
+	ADCSRA |= (1 << ADSC);		/* start first conversion */
+}
+
+int main(void) {
+	clock_prescale_set(clock_div_16);
+
+	uint8_t dummy;
+
+	initUSART();
+	initFreerunningADC();
+	
+	while (1) {
+		//transmitByte(ADCH);	
+		dummy = ADCH;
+		transmitByte(ADCL);	
+		_delay_ms(SAMPLE_DELAY);
+	}
+
+	return (0);
+}
